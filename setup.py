a3d9a5c01b5d15df3006b65eac6f387eb0e427f1
kamil <kamil.wojciechowski@chain-insights.ai>
2024-05-31 12:50:41 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sn15_subnet_shared_libs',
-    version='1.0.0',
+    name='blockchain-data-subnet-shared-libs',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         # List your package dependencies here
@@ -11,12 +11,12 @@ setup(
     ],
     author='Dmytro Savenkov',
     author_email='dmytro.savenkov@chain-insights.ai',
-    description='All shared libs that all components, executables, and services of Sn15 are based on',
-    url='https://github.com/YOUR_USERNAME/YOUR_REPOSITORY',
+    description='All shared libs that all components, executables, and services of blockchain data subnet are based on',
+    url='https://github.com/blockchain-insights/blockchain-data-subnet-shared-libs',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.10',
 )
