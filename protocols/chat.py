cb3e8ac7e02f09f7983c467c80e221d8701d0100
kamil <kamil.wojciechowski@chain-insights.ai>
2024-06-02 13:55:47 +0200

    addded descriptions to chat dtos

--- protocols/chat.py
+++ protocols/chat.py
@@ -1,24 +1,24 @@
 from uuid import UUID
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 from enum import Enum
 from protocols.llm_engine import QueryOutput
 
 
 class ChatMessageRequest(BaseModel):
-    network: str
-    prompt: str
+    network: str = Field(..., description="network name")
+    prompt: str = Field(..., description="user prompt")
 
 
 class ChatMessageVariantRequest(BaseModel):
-    network: str
-    prompt: str
-    miner_hotkey: str
+    network: str = Field(..., description="network name")
+    prompt: str = Field(..., description="user prompt")
+    miner_hotkey: str = Field(..., description="miner hotkey")
 
 
 class ChatMessageResponse(BaseModel):
-    miner_hotkey: Optional[str] = None
-    response: List[QueryOutput]
+    miner_hotkey: Optional[str] = Field(None, description="miner hotkey")
+    response: List[QueryOutput] = Field(..., description="response from llm engine")
 
 
 class ContentType(str, Enum):
